7f3209dcf1f7b855dc086f949eeea2c647880de1
Sakshi533576 <sakshimaurya1221@gmail.com>
2026-01-09 14:48:01 +0530

    Add files via upload

---
+++ exception.py
@@ -0,0 +1,40 @@
+
+#error exception
+# try:    
+#     x=int("abc")
+# except ValueError:
+#     print("value error occurred")
+# except TypeError:
+#     print("Type error occurred")
+
+# def main():
+#     try:
+#         print("1. Addition")
+#         print("2. Subtraction")
+#         print("3. Multiply")
+#         print("4. Devision")
+#         choice=int(input("enter your choice(1/2/3/4)"))
+#         x=int(input("enter number 1"))
+#         y=int(input("enter number2"))
+#         if choice==1:
+#             result=x+y
+#             print(result)
+#         elif choice==3:
+#             result=x*y
+#             print(result)
+#         elif choice==4:
+#             if y!=0:
+             
+#              result=x/y
+#         else:
+#             print("can't be dEvide by 0")
+# except ZeroDivisionError:
+
+
+
+# def main():
+#     print("hello world")
+# if __name__=="__main__":
+#     main()
+# print("Maurya900")
+# print("value in built variable name is: ",__name__)
