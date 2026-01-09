64dc739911f54f6f522304541852fad053a872d9
Sakshi533576 <sakshimaurya1221@gmail.com>
2026-01-09 14:48:23 +0530

    Add files via upload

---
+++ Moduls.py
@@ -0,0 +1,55 @@
+# import math
+# print (math.sqrt(9))
+# print(math.pi)
+# print(math.pow(2,4))
+# print(math.factorial(6))
+
+# import datetime
+# now=datetime.datetime.now()
+# print(now)
+# print(now.year)
+# print(now.month)
+# print(now.day)
+# today=datetime.date.today()
+# print(today)
+# future=today+datetime.timedelta(days=15)
+# print("15 days later:",future)
+# import sys
+# print(sys.version)
+# print(sys.executable)
+# print(sys.platform)
+
+# import random
+# print(random.randint(1,10))
+# print(random.random())
+# print(random.choice(["AIPA","CSA","COPA"]))
+# trade=["AI","COPA","EM","DM"]
+# random.shuffle(trade)
+# print(trade)
+
+# import os
+# # print(os.getcwd())
+# # print(os.listdir())
+# os.rmdir("shusmita")
+
+# current time
+# import time
+# print(time.ctime())
+
+#Practice
+# import math
+#Question 1
+# r=pow(3,2)
+# print(math.pi*r)
+
+#Question 2
+# print(math.factorial((int(input("enter factorial")))))
+# print(math.sqrt(int(input("enter sqrt:"))))
+# import random
+#Question 3
+# print("and the number is...",random.choices(["1","2","3","4","5"]))
+#Question 4
+# print("guessing game:",random.choices([1,2,3,4,5,6.7]))
+
+
+
