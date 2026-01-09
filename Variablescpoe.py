20e06509fd0c03a23895f1e7b1e43e4bd8449878
Sakshi533576 <sakshimaurya1221@gmail.com>
2026-01-09 14:48:49 +0530

    Add files via upload

---
+++ Variablescpoe.py
@@ -0,0 +1,71 @@
+#Global Variable
+# college="NSTIW"
+# def globals():
+#     college="Allahabad"
+#     print(college)
+# globals()
+# print(college)
+#Local variables
+# def local():
+#     college="NSTIW"
+#     print(college)
+# local()
+
+# 
+
+#Global keyword
+# Name="Sejal"
+# def gk():
+#     global Name
+#     Name="Akriti"
+#     print(Name)
+# gk()
+# print(Name)
+
+#Student name
+# def local():
+#     student="Adit"
+#     print(student)
+# # local()
+# def customer1():
+#     customer1="Hariram"
+#     print(customer1)  
+# def customer2():
+#     customer2="syamsundar"
+#     print(customer2)
+# customer1()
+# customer2()
+#
+# course="python"
+# def show_course():
+#     show_course="Mathmatics"
+#     print(show_course)
+# show_course()
+# print(course)
+
+# institute="NSTIW Allahabaad"
+# def welcome():
+#    welcome="Hamare institute me aap ka swagat hai"
+#    print(welcome)
+# def info():
+#     info="kripya chappal bahar nikal de!"
+#     print(info)
+# welcome()
+# info()
+# #
+# count=0
+# def increase():
+# global count
+#     count+=1
+#     print(count)
+# increase()
+
+#Factorial
+# def factorial(n):
+#     if n==0:
+#         return 1
+#     else:
+#         return n*factorial(n-1)
+# num=int(input("enter number:"))
+# print("factorial of",num,"is:",factorial(num))
+#practice
