5726f8832039d7de3cfc446b13fc45c25e1435d3
Sakshi533576 <sakshimaurya1221@gmail.com>
2026-01-09 14:47:46 +0530

    Add files via upload

---
+++ f sting.py
@@ -0,0 +1,19 @@
+print(f"sum of 5 + 3 is:{5+3}")
+x=2
+y=5
+c=x+y
+print(f"the sum of {x} and {y} is {c}")
+x=5
+y=9
+z=15
+c=x+y
+s=c+z
+print("sum of",x,"and",y,"is",z)
+print("sum of",c,"and",z,"is",s)
+
+print(f"The result of {15}-{4} is:{15-4}")
+print(f"The result of {15}+{4} is:{15+4}")
+print(f"The result of {15}*{4} is:{15*4}")
+print(f"The result of {15}/{4} is:{15/4}")
+print(f"The result of {15}**{4} is:{15**4}")
+print(f"The result of {15}//{4} is:{15//4}")
